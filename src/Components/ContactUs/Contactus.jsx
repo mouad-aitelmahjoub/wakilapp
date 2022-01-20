410ad54676f032c95e1288e89f9a0753f14c4e7b
ali-aitelmahjoub <aitelmahjoubali6@gmail.com>
2022-01-20 13:33:15 +0000

    modification in contact us page add animation to the inputs

--- src/Components/ContactUs/Contactus.jsx
+++ src/Components/ContactUs/Contactus.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './contactus.css'
+import { motion } from 'framer-motion';
 
 
 const Contactus = () => {
@@ -12,26 +13,50 @@ return <div className='contactUsContainer'>
                                 <p className='contactus-para'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi id dolorum expedita.</p>
                             </div>
                             <form className="boottomLeft">
-                               <div className="firstBlock block">
+                               <motion.div
+                               whileInView={{y:0,opacity:1}}
+                               initial={{y:100,opacity:0}}
+                               transition={{delay:1,duration:1}}
+                               className="firstBlock block">
                                <input type="text" className='txt-nom txt' placeholder='Votre Nom' />
                                 <input type="text" className='txt-prenom txt' placeholder='Votre Prenom' />
-                               </div>
+                               </motion.div>
 
-                                  <div className="secondBlock block">
+                                  <motion.div 
+                                   whileInView={{y:0,opacity:1}}
+                                   initial={{y:100,opacity:0}}
+                                   transition={{delay:1,duration:1.3}}
+                                  className="secondBlock block">
                                   <input type="email" className='txt-email txt' placeholder='Votre email' />
-                                </div>
+                                </motion.div>
 
-                               <div className="thirdBlock block">
+                               <motion.div
+                                whileInView={{y:0,opacity:1}}
+                                initial={{y:100,opacity:0}}
+                                transition={{delay:1,duration:1.5}}
+                               className="thirdBlock block">
                                <input type="tel" className='txt-tel txt' placeholder='Votre Numero de telephone' />
-                               </div>
+                               </motion.div>
 
-                                <div className="fourthBlock block">
+                                <motion.div
+                                 whileInView={{y:0,opacity:1}}
+                                 initial={{y:100,opacity:0}}
+                                 transition={{delay:1,duration:1.7}}
+                                className="fourthBlock block">
                                 <input type="text" className='txt-agence txt' placeholder='Agence ou resau'/>
                                 <input type="number" className='rdv-souhaite txt' placeholder='nombre de Rendez-vous souhautes' />
-                                </div>
+                                </motion.div>
 
-                                <textarea className='txt-besoin txt block' id="" cols="30" rows="10" placeholder='Besoin specifiques'></textarea>
-                                <button type="submit" className='btn-submit block'>Envoyer</button>
+                                <motion.textarea
+                                 whileInView={{y:0,opacity:1}}
+                                 initial={{y:100,opacity:0}}
+                                 transition={{delay:1,duration:1.9}}
+                                className='txt-besoin txt block' id="" cols="30" rows="10" placeholder='Besoin specifiques'></motion.textarea>
+                                <motion.button
+                                 whileInView={{y:0,opacity:1}}
+                                 initial={{y:80,opacity:0}}
+                                 transition={{delay:1,duration:2.2}}
+                                type="submit" className='btn-submit block'>Envoyer</motion.button>
                             </form>
                         </div>
                         <div className="right">
